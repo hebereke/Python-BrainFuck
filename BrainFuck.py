d33a0b1f4bdc5bf56df7b8855f67eb1e1a4236f8
Hebereke <kurosawa@mbb.nifty.com>
2018-07-20 20:15:16 +0900

    - improve lexer - add bfcode method

--- BrainFuck.py
+++ BrainFuck.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## Python3
 
 import sys
 
 class OpToken(dict):
-    """OpToken class to store transrate map
-    """
+	"""OpToken class to store transrate map
+	"""
 	def __init__(self, optokendict={}):
 		dict.__init__(self, optokendict)
 	def tokens(self): ## output token list
-		return self.values()
+		return list(self.values())
 	def opcodes(self): ## output opcode list
-		return self.keys()
+		return list(self.keys())
 	def sorted_tokens(self): ## sorted tokens by length
 		return sorted(self.tokens(), key=lambda x: len(x), reverse=True)
 	def swap(self): ## return new OpToken object to swap token and opcode
@@ -25,7 +25,7 @@ class OpToken(dict):
 
 class BrainFuck:
 	"""BrainFuck class to generate BrainFuck Variants
-    http://www.muppetlabs.com/~breadbox/bf/
+	http://www.muppetlabs.com/~breadbox/bf/
 	"""
 	BF_OPTOKEN_DICT = dict( # BrainFuck opcode and token
 		nxt = '>',
@@ -47,8 +47,8 @@ class BrainFuck:
 		"""parameter description:
 		optoken_dict = dict of op and token
 		asize  = cell array size
-        sep = separator of src code
-        token = token list to replace with original token, easy to create variant
+		sep = separator of src code
+		token = token list to replace with original token, easy to create variant
 		"""
 		self.asize = asize
 		self.sep = sep
@@ -80,18 +80,24 @@ class BrainFuck:
 		tokens=[]
 		cur=0
 		clen=1
+		tokenlist.sort(key=lambda x: len(x), reverse=True)
+		maxtlen=len(tokenlist[0])
 		while cur <= len(src)-1:
 			end=cur+clen
 			s=src[cur:end]
 			if end > len(src):
 				break
-			if s in tokenlist:
+			elif clen > maxtlen:
+				cur+=1
+				clen=1
+				continue
+			elif s in tokenlist:
 				tokens.append(s)
 				cur+=len(s)
 				clen=1
-			elif s==' ':
+			elif s==sep:
 				cur+=1
-			elif re.match('\s', src):
+			elif re.match('\s', s):
 				cur+=1
 			else:
 				clen+=1
@@ -118,55 +124,37 @@ class BrainFuck:
 		return True
 
 	def initialize(self):
-        """initialize before running
-        """
+		"""initialize before running
+		"""
 		self.ptr = 0 # pointer in executer()
 		self.cur = 0 # index of codes
 		self.cell = [0 for i in range(self.asize)] # cell
 
-	def prepro(self, opcodes):
-        """pre-processing
-        """
+	def prepro(self, opcodes): ## pre-processing
 		return opcodes
 
-	def postpro(self, opcodes):
-        """post-processing
-        """
+	def postpro(self, opcodes): ## post-processing
 		return opcodes
 
-	def op_nxt(self, opcodes):
-        """increment pointer (++ptr)
-        """
+	def op_nxt(self, opcodes): ## increment pointer (++ptr)
 		self.ptr+=1
 
-	def op_prv(self, opcodes):
-        """decrement pointer (--ptr)
-        """
+	def op_prv(self, opcodes): ## decrement pointer (--ptr)
 		self.ptr-=1
 
-	def op_inc(self, opcodes):
-        """increment the byte at pointer (++*ptr)
-        """		self.cell[self.ptr]+=1
+	def op_inc(self, opcodes): ## increment the byte at pointer (++*ptr)
+		self.cell[self.ptr]+=1
 
-	def op_dec(self, opcodes):
-        """decrement the byte at pointer (--*ptr)
-        """		self.cell[self.ptr]+=1
+	def op_dec(self, opcodes): ## decrement the byte at pointer (--*ptr)
 		self.cell[self.ptr]-=1
 
-	def op_put(self, opcodes):
-        """output the byte at the pointer (putchar(*ptr))
-        """
-		self.cell[self.ptr]+=1
+	def op_put(self, opcodes): ## output the byte at the pointer (putchar(*ptr))
 		sys.stdout.write(chr(self.cell[self.ptr]))
 
-	def op_get(self, opcodes):
-        """Input a byte and store it in the byte at the pointer (*ptr = getchar())
-        """
+	def op_get(self, opcodes): ## input a byte and store it in the byte at the pointer (*ptr = getchar())
 		self.cell[self.ptr] = ord(raw_input("Enter>")[0])
 
-	def op_opn(self, opcodes):
-        """jump forward past the matching ] if the byte at the pointer is zero (which (*ptr) {)
-        """
+	def op_opn(self, opcodes): ## jump forward past the matching ] if the byte at the pointer is zero (which (*ptr) {)
 		if self.cell[self.ptr] != 0:
 			return False
 		level=1
@@ -175,9 +163,7 @@ class BrainFuck:
 			if opcodes[self.cur]=='opn': level+=1
 			if opcodes[self.cur]=='cls': level-=1
 
-	def op_cls(self, opcodes):
-        """jump backward to the matching [ unless the byte at the pointer is zero (})
-        """
+	def op_cls(self, opcodes): ## jump backward to the matching [ unless the byte at the pointer is zero (})
 		if self.cell[self.ptr] == 0:
 			return False
 		level=1
@@ -208,12 +194,18 @@ class BrainFuck:
 		"""output src from opcodes list
 		"""
 		return self.sep.join(self.opcode2tokens(opcodes))
-		
+
 	def converter(self, src):
 		"""convert original BrainFuck code to variant code
 		"""
 		lex=self.lexer(src, self.BF_OPTOKEN.tokens())
 		return self.sep.join(self.translator(lex, dict(zip(self.BF_OPTOKEN.tokens(), self.optoken.tokens()))))
+
+	def bfcode(self, src):
+		"""output BrainFuck code
+		"""
+		op=self.opcodes(src)
+		return ''.join(self.translator(op, self.BF_OPTOKEN))
 
 	def hello_world(self):
 		"""print "Hello World!" code and the result
@@ -224,17 +216,17 @@ class BrainFuck:
 		return self.run(src)
 
 class Ook (BrainFuck):
-    """Ook! language
-    http://www.dangermouse.net/esoteric/ook.html
-    """
+	"""Ook! language
+	http://www.dangermouse.net/esoteric/ook.html
+	"""
 	OOK_TOKEN=['Ook. Ook?','Ook? Ook.','Ook. Ook.','Ook! Ook!','Ook. Ook!','Ook! Ook.','Ook! Ook?','Ook? Ook!']
 	def __init__(self, tokens=OOK_TOKEN):
 		BrainFuck.__init__(self, tokens=tokens, sep=' ')
 
 class BrainCrash (BrainFuck):
-    """BrainCrash language
-    https://enpedia.rxy.jp/wiki/BrainCrash
-    """
+	"""BrainCrash language
+	https://enpedia.rxy.jp/wiki/BrainCrash
+	"""
 	BC_OPTOKEN_DICT={
 		'or':'|',
 		'and':'&',
@@ -268,8 +260,8 @@ class BrainCrash (BrainFuck):
 		self.cell[self.ptr+1]= ~ self.cell[self.ptr]
 
 class CommDisCore(BrainCrash):
-    """extend class for CommDis language
-    """
+	"""extend class for CommDis language
+	"""
 	CDC_OPTOKEN_DICT=dict(
 		shl = '*',
 		shr = '/',
@@ -297,15 +289,14 @@ class CommDisCore(BrainCrash):
 		self.ptr=0
 
 class CommDis(CommDisCore):
-    """コミュ障プログラミング言語
-    http://www.moonroom.mydns.jp/ls/software/commdis.htm
-    """
+	"""コミュ障プログラミング言語
+	http://www.moonroom.mydns.jp/ls/software/commdis.htm
+	"""
 	CD_TOKEN=['ｱｱ…','ｱｱ､','ｱ…','ｱ､','ｴｯﾄ…','ｴｯﾄ､','ｻｾﾝ…','ｯｽ…','ｱｯ…','ｱｯ､','ｱﾉ…','ｱﾉ､','ｱｰ…','ｱｰ､','ｴ…','ｴ､','ｴｯ…','ｴｯ?']
 	def __init__(self, tokens=CD_TOKEN):
 		CommDisCore.__init__(self, tokens=tokens)
 
 def test_bf():
-	# BrainFuck class
 	print('BrainFuck class:')
 	b=BrainFuck()
 	b.hello_world()
@@ -314,26 +305,44 @@ def test_bf():
 	print('')
 
 def test_bc():
-	# BrainCrash class
 	print('BrainCrash class:')
 	c=BrainCrash()
 	c.run('')
 	print('')
 	c.run('>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.')
-	c.run('[-]&&&&&&&&&&&&&+++++++[>++++++++++<-]>-.<+++++++[>++++++<-]>-.++.-----------.-.+++++.--------')
+	c.run('[-]&&&&&&&&&&&&&+++++++[>++++++++++<-]>-.<+++++++[>++++++<-]>-.++.-----------.-.+++++.--------') # output 'Enpedia'
 	print('')
 	print('')
 
 def test_cd():
-	# CommDis class
 	print('CommDis class:')
 	cd=CommDis()
 	cd.hello_world()
 	print(cd.opcode2tokens(['nxt','prv','hom','pjm','cls']))
 	print('')
+	fizzbuzzsrc="""ｱ…ｱ…ｱ…ｱ…ｱ…ｱ…ｻｾﾝ…ｱ､ｱｱ…ｱ…ｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱ…ｱｱ…ｱ…
+ｱｱ…ｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｯｽ…ｱｱ…ｻｾﾝ…ｱｱ､ｱ…ｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱ…
+ｱ…ｱ…ｱｱ…ｱ…ｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱ…ｱ…ｱ…ｱｱ…ｱ…ｱ…ｱ…ｱ…ｱ…
+ｱｱ…ｱ…ｱ…ｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱ…
+ｱ…ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱ､ｯｽ…ｱｱ､ｱ…ｱ…
+ｱ…ｱ…ｱｱ…ｱ…ｱ…ｱ…ｱｱ…ｱ､ｱ､ｱｱ…ｱ…ｱ…ｱ…ｱｱ…ｱ､ｱｱ…ｱｱ…ｱ､ｱ､ｱ､
+ｱｱ…ｱ…ｱ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱ…ｱ…ｱ…ｱ…ｻｾﾝ…ｱ､ｱｱ…ｱ…ｱ…ｱｱ…ｱ…
+ｱ…ｱｱ､ｱｱ､ｯｽ…ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｻｾﾝ…ｱ､ｱｱ…ｱ､ｻｾﾝ…
+ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｯｽ…ｱｱ…ｻｾﾝ…ｱｱ､ｱ…ｱ…ｱ…ｱｱ…ｴｯﾄ…
+ｱｱ…ｴｯﾄ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｴｯﾄ…ｴｯﾄ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱｱ､ｯｽ…ｱｱ､
+ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱ､ｻｾﾝ…ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｯｽ…ｱｱ…ｻｾﾝ…ｱｱ､ｱ…ｱ…ｱ…
+ｱ…ｱ…ｱｱ…ｴｯﾄ…ｱｱ…ｴｯﾄ…ｱｱ…ｴｯﾄ…ｴｯﾄ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱｱ､ｯｽ…
+ｱｱ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱｱ､ｱ､ｻｾﾝ…ｱｱ､ｱｱ､ｱｱ､ｯｽ…ｱｱ､ｻｾﾝ…ｻｾﾝ…ｱ､ｱｱ､
+ｱｱ､ｱ…ｱｱ…ｱｱ…ｯｽ…ｱｱ…ｱｱ…ｱｱ…ｱ…ｱｱ…ｱ…ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｻｾﾝ…
+ｱ､ｱｱ…ｱｱ…ｱ…ｱｱ…ｱ…ｱｱ…ｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｯｽ…ｱｱ､ｯｽ…ｱｱ…ｱｱ…ｻｾﾝ…
+ｻｾﾝ…ｱ､ｯｽ…ｱｱ､ｯｽ…ｱｱ…ｻｾﾝ…ｱｱ…ｱｱ…ｱｱ…ｻｾﾝ…ｱｱ…ｴｯﾄ…ｱｱ､ｱｱ､ｴｯﾄ…
+ｱｱ､ｱｱ､ｱｱ､ｯｽ…ｱｱ､ｻｾﾝ…ｴｯﾄ…ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｯｽ…ｱｱ…ｯｽ…ｱｱ…ｴｯﾄ…
+ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｱｱ､ｯｽ…"""
+#	cd.run(fizzbuzzsrc)
+	print(cd.bfcode(fizzbuzzsrc))
+	print('')
 
 def test_ook():
-	# Ook class
 	print('Ook class:')
 	o=Ook()
 	o.hello_world()
@@ -350,3 +359,4 @@ if __name__ == "__main__":
 	print('kapibara variant:')
 	k=BrainFuck(tokens=['のすのす','もでーん','キュルッ！','もふっ！', 'むぎゅっと','グッ！！','ぬっくし','うっとり'], sep=' ')
 	k.hello_world()
+	# JoJo language
