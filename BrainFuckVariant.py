f217ff9fc85330d884a7f6e8b183896fdcf81ce9
Hebereke <kurosawa@mbb.nifty.com>
2018-07-26 13:05:14 +0900

    - correct wrong reference link of KQ language

--- BrainFuckVariant.py
+++ BrainFuckVariant.py
@@ -733,7 +733,7 @@ def test_misa():
 ## プログラミング言語 KQ
 class KQ(BrainFuck.BrainFuck):
     """プログラミング言語 KQ
-    https://enpedia.rxy.jp/wiki/Misa_(%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E8%A8%80%E8%AA%9E)
+    https://enpedia.rxy.jp/wiki/KQ_(%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E8%A8%80%E8%AA%9E)
     """
     OPTOKEN_DICT = dict( # BrainFuck opcode and token
         nxt = ['ﾀﾞｧｲｪｽ', 'ｼｪﾘ'],
